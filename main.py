7e67eae451e133fc4d68f8d82d470acd7e351d1d
Mazharabbai <Mazharabbasi7007@gmail.com>
2025-05-06 13:59:17 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-‎javascript: (() => {  var uid2 = prompt("UID IG : ");  function ClientMutation() { return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16) ); }  var url = 'https://accountscenter.facebook.com/api/graphql/'; var dtsg = require(["DTSGInitData"]).token; var userid = require(["CurrentUserInitialData"]).USER_ID; var variables ='{"client_mutation_id":"'+ClientMutation()+'","accounts_to_sync":["'+uid2+'","'+userid+'"],"resources_to_sync":["NAME","PROFILE_PHOTO"],"resources_to_unsync":null,"scale":3,"source_of_truth_array":[{"resource_source":"IG"},{"resource_source":"FB"}],"source_account":"'+userid+'","family_device_id":"device_id_fetch_datr","username_unsync_params":null,"platform":"FACEBOOK","sync_logging_params":{"client_flow_type":"IM_SETTINGS"},"interface":"FB_WEB","feta_profile_sync":false}'; var data = "fb_dtsg="+dtsg+"&__user="+userid+"&variables="+variables+"&av="+userid+"&fb_api_req_friendly_name=useFXIMUpdateNameMutation&fb_api_caller_class=RelayModern&server_timestamps=true&doc_id=9388416374608398";  fetch(url, { method: 'POST', body: data, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).then(resp => resp.text()).then(resp => {if (resp.includes('error')) { alert(resp) } else {alert("GAGAL")}})  })();
-‎
+# coding=utf-8
+import json
+import os
+import sys
+import shutil
+from PIL import Image
+
+try:
+    name = sys.argv[1]
+    atlas = open(name, 'r')
+    data = json.loads(atlas.read())
+    image_name = data['meta']['image']
+    path = data['meta']['prefix']
+    print image_name
+    print path
+    # 删除文件夹
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    # 创建文件夹
+    os.makedirs(path)
+    # 加载图片
+    im = Image.open(image_name)
+    frames = data['frames']
+    for frame in frames:
+        frame_info = frames[frame]
+        x = frame_info['frame']['x']
+        y = frame_info['frame']['y']
+        w = frame_info['frame']['w']
+        h = frame_info['frame']['h']
+        print frame, x, y, w, h
+        box = (x, y, x + w, y + h)
+        region = im.crop(box)
+        region.save(path + frame)
+
+except IOError:
+    print '未找到文件'
+else:
+    atlas.close()
